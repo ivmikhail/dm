090fbc80c41cf75831276454383b62324973e4da
Mikhail Ivanov <ivmikhail@gmail.com>
2019-10-05 17:16:14 +0300

    Fail test for checking CI

--- src/test/java/com/github/ivmikhail/dm/AppTest.java
+++ src/test/java/com/github/ivmikhail/dm/AppTest.java
@@ -11,6 +11,7 @@ import java.io.PrintStream;
 import java.security.Permission;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AppTest {
     private SecurityManager originalSecurityManager;
@@ -55,6 +56,7 @@ public class AppTest {
         App.main(new String[]{f.getAbsolutePath()});//pass empty file
 
         assertEquals("Start download data from []\n", outContent.toString());
+        fail();
         f.delete();
     }
 }
